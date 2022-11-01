e7870f86131e389d271cd2349ddb51849180945d
jneiracampos <j.neira@uniandes.edu.co>
2022-11-01 13:44:10 -0500

    Taller Angular

---
+++ src/app/series/series.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { SeriesService } from './series.service';
+import { Serie } from './serie';
+
+@Component({
+  selector: 'app-series',
+  templateUrl: './series.component.html',
+  styleUrls: ['./series.component.css']
+})
+export class SeriesComponent implements OnInit {
+
+  constructor(private SeriesService: SeriesService ) { }
+
+  series: Array<Serie> = [];
+  averageSeasons = 0;
+
+  ngOnInit()
+  {
+    this.getSeries();
+  }
+
+  ngDoCheck()
+  {
+    this.getAverageSeasons(this.series);
+  }
+
+  getSeries()
+  {
+    this.SeriesService.getSeries().subscribe(series => {
+      this.series = series;
+    });
+  }
+
+  getAverageSeasons(series: Array<Serie>)
+  {
+    let totalSeasons: number = 0;
+    series.forEach(serie => {
+      totalSeasons += serie.seasons;
+    });
+    this.averageSeasons = totalSeasons / series.length;
+  }
+
+
+}
